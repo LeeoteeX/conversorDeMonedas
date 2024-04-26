7e18f178ea946280aad37150390d813ca4ceeb1a
Leandro Gramajo <pilincho93@gmail.com>
2024-04-26 11:28:02 -0300

    Hora formateada

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,5 @@
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,6 +11,7 @@ public class Menu {
         ConsultaDatos consulta = new ConsultaDatos();
         Scanner teclado = new Scanner(System.in);
         List<Solicitud> listaSolicitudes = new ArrayList<>();
+        DateTimeFormatter formatearHora = DateTimeFormatter.ofPattern("HH:mm:ss");
         do {
             System.out.println("""
                 
@@ -33,35 +35,35 @@ public class Menu {
             switch (opc){
                 case 1:
                     System.out.println("El valor de 1 Dólar son " + consulta.consulta("USD","ARS") + " Pesos Argentinos.");
-                    listaSolicitudes.add(new Solicitud(LocalTime.now(),"USD","ARS"));
+                    listaSolicitudes.add(new Solicitud(LocalTime.parse(LocalTime.now().format(formatearHora)),"USD","ARS"));
                 break;
                 case 2:
                     System.out.println("El valor de 1 Peso Argentino son " + consulta.consulta("ARS", "USD") + " Dólares");
-                    listaSolicitudes.add(new Solicitud(LocalTime.now(),"ARS","USD"));
+                    listaSolicitudes.add(new Solicitud(LocalTime.parse(LocalTime.now().format(formatearHora)),"ARS","USD"));
                 break;
                 case 3:
                     System.out.println("El valor de 1 Dólar son " + consulta.consulta("USD","RSD") + " Reales.");
-                    listaSolicitudes.add(new Solicitud(LocalTime.now(),"USD","RSD"));
+                    listaSolicitudes.add(new Solicitud(LocalTime.parse(LocalTime.now().format(formatearHora)),"USD","RSD"));
                 break;
                 case 4:
                     System.out.println("El valor de 1 Real es de " + consulta.consulta("RSD","USD") + " Dólares.");
-                    listaSolicitudes.add(new Solicitud(LocalTime.now(),"RSD","USD"));
+                    listaSolicitudes.add(new Solicitud(LocalTime.parse(LocalTime.now().format(formatearHora)),"RSD","USD"));
                 break;
                 case 5:
                     System.out.println("El valor de 1 Peso Argentino son " + consulta.consulta("ARS","CLP") + " Pesos Chilenos.");
-                    listaSolicitudes.add(new Solicitud(LocalTime.now(),"ARS","CLP"));
+                    listaSolicitudes.add(new Solicitud(LocalTime.parse(LocalTime.now().format(formatearHora)),"ARS","CLP"));
                 break;
                 case 6:
                     System.out.println("El valor de 1 Peso Chileno es " + consulta.consulta("CLP","ARS") + " Pesos Argentinos.");
-                    listaSolicitudes.add(new Solicitud(LocalTime.now(),"CLP","ARS"));
+                    listaSolicitudes.add(new Solicitud(LocalTime.parse(LocalTime.now().format(formatearHora)),"CLP","ARS"));
                 break;
                 case 7:
                     if (listaSolicitudes.isEmpty()){
                         System.out.println("No ha realizado ninguna solicitud.");
                     } else {
                         for (Solicitud solicitud : listaSolicitudes){
-                            System.out.println("Ha realizado una solicitud para convertir de " + solicitud.getMonedaOrigen() + " a" +
-                                    " " + solicitud.getMonedaComparar() + " a las : " + solicitud.getHora());
+                            System.out.println("Ha realizado una solicitud para convertir de " + solicitud.getMonedaOrigen() + " a "
+                                    + solicitud.getMonedaComparar() + " a las : " + solicitud.getHora());
                         }
                     }
                     break;
